2e69789fefa08904e128474ad5978085bdc0a4e8
Osei <williamsabodunrin@gmail.com>
2024-02-22 23:55:22 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,11 @@ class DB:
 
     def add_user(self, email, hashed_password):
         """adds a user."""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
