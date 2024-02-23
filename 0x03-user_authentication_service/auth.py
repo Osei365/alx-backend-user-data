f90d9d9a1e8aee7ed0410884be6821196e7fb017
Osei <williamsabodunrin@gmail.com>
2024-02-23 02:01:52 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,6 +61,16 @@ class Auth:
         """ destroys a session."""
         self._db.update_user(user_id, session_id=None)
 
+    def get_reset_password_token(self, email: str) -> str:
+        """resets password."""
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except Exception:
+            raise ValueError()
+
 
 def _hash_password(password: str) -> bytes:
     """hases a password."""
