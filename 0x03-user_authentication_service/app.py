81d2ef2f77b970b40f3b362e13c058dd7e222db7
Osei <williamsabodunrin@gmail.com>
2024-02-23 03:49:41 +0100

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,9 +86,9 @@ def update_password():
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
     except Exception:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
